395625e81d2470c5bb0d2716728d3e000856d5ed
Paulo Henrique Chi <paulo.henrique.chi@outlook.com>
2024-02-29 21:08:49 -0300

    functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("sandbox");
+let greet;
+greet = () => {
+    console.log("hello");
+};
+const sum = (a, b, c = 0) => {
+    return a + b + c;
+};
+const minus = (a, b) => {
+    return a - b;
+};
+const noReturn = () => {
+    console.log("void return");
+};
+sum(10, 20);
+minus(10, 5);
+noReturn();
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,21 @@
-console.log("sandbox");
+let greet: Function;
+
+greet = () => {
+  console.log("hello");
+};
+
+const sum = (a: number, b: number, c: number = 0) => {
+  return a + b + c;
+};
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+const noReturn = (): void => {
+  console.log("void return");
+};
+
+sum(10, 20);
+minus(10, 5);
+noReturn();
